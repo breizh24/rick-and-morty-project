d6f1900e165f1a0732dcccbde90edba3c68ca774
Alberico <alberico.zitiello@outlook.it>
2023-05-19 22:18:35 +0200

    setup styled components

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { type PropsWithChildren } from 'react';
+import StyledComponentsRegistry from '@/utils/StyledComponentsRegistry';
 
 export default function Providers({ children }: PropsWithChildren) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </QueryClientProvider>
   );
 }
